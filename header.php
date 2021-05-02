bbe0ec7afb3a63a2029f7329d7d9d15161ed0a03
greydelta <55234616+greydelta@users.noreply.github.com>
2021-05-02 14:30:00 +0800

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+    <header>
+        <div>
+            <nav aria-label="Header Nav">
+                <a href="blog.php">Blog</a>
+                &nbsp;
+                &nbsp;
+                <a href="login.php">Sign in</a>
+            </nav>
+        </div>
+    </header>
+
