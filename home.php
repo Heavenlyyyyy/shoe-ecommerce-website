2f9c0b15d02aef776416820dd6bd08971fcffae4
greydelta <55234616+greydelta@users.noreply.github.com>
2021-05-04 00:00:00 +0800

    Header styles

--- home.php
+++ home.php
@@ -5,9 +5,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,inital-scale=1.0">
     <title>Urban Shoes Online Shop | Urban Shoes MY</title>
+    <style>
+        <?php include('style.php'); ?>
+        .content .container1 {
+            width: 100%;
+        }
+
+        .container1 img {
+            width: 100%;
+            height: 800px;
+        }
+
+        .container1 .shop {
+            position: absolute;
+            top: 60%;
+            left: 35%;
+            transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+            background-color: black;
+            color: whitesmoke;
+            font-size: 16px;
+            padding: 12px 24px;
+            cursor: pointer;
+            border-radius: 5px;
+            text-align: center;
+        }
+
+        .container1 .shop:hover {
+            border: 1.5px solid white;
+            background-color: rgb(50, 50, 50);
+            color: white;
+        }
+    </style>
 </head>
 
 <body>
+    <?php include('header.php'); ?>
 </body>
 
 </html>
