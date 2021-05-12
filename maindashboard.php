7dcf686c9bce9569ea1a5e7fc78a8e99112b9d47
greydelta <55234616+greydelta@users.noreply.github.com>
2021-05-13 00:00:00 +0800

    Create maindashboard.php

---
+++ maindashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,inital-scale=1.0">
+    <link rel="shortcut icon" href="images/Logo.ico" />
+    
+    <title>Urban Shoes Online Shop | Shop</title>
+</head>
+
+<body>
+    <?php include('header.php'); ?>
+    <div class="wrapper hovercollapse">
+        <?php include('navbar.php'); ?>
+    </div>
+    <?php include('footer.php'); ?>
+    <script src="scripts.js"></script>
+</body>
+
+</html>
