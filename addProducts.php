bec9245b089fad9c3ac635b3680939f8b83559f7
greydelta <55234616+greydelta@users.noreply.github.com>
2021-06-13 00:00:00 +0800

    Update addProducts.php

--- addProducts.php
+++ addProducts.php
@@ -2,9 +2,88 @@
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,inital-scale=1.0">
+    <link rel="shortcut icon" href="images/Logo.ico" />
+    <title>(Admin) Manage Products</title>
+    <style>
+        <?php include 'styles.css'; ?>
+        input[type=text],select{
+            width: 100%;
+            /* Full width */
+            padding: 12px;
+            /* Some padding */
+            border: 1px solid #ccc;
+            /* Gray border */
+            border-radius: 4px;
+            /* Rounded borders */
+            box-sizing: border-box;
+            /* Make sure that padding and width stays in place */
+            margin-top: 6px;
+            /* Add a top margin */
+            margin-bottom: 16px;
+            /* Bottom margin */
+            resize: vertical
+                /* Allow the user to vertically resize the textarea (not horizontally) */
+        }
+		
+		table {
+			border-collapse: collapse;
+			width: 100%;
+            /* Full width */
+            padding: 12px;
+            /* Some padding */
+            border: 1px solid #ccc;
+            /* Gray border */
+            border-radius: 4px;
+            /* Rounded borders */
+            box-sizing: border-box;
+            /* Make sure that padding and width stays in place */
+            margin-top: 6px;
+            /* Add a top margin */
+            margin-bottom: 16px;
+            /* Bottom margin */
+            resize: vertical
+                /* Allow the user to vertically resize the textarea (not horizontally) */
+		}
+		td, th {
+			border: 1px solid #ccc;
+			padding: 10px;
+		}
+		th:empty {
+			border: 0;
+		}
+
+        button[type=submit]:hover {
+            background-color: lightgrey;
+        }
+
+        button[type=submit] {
+            background-color: grey;
+            color: white;
+            padding: 12px 20px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+    </style>
 </head>
 
 <body>
+    <?php include('header.php'); ?>
+    <div class="wrapper hovercollapse">
+        <?php include('navbar.php'); ?>
+        <?php include('sidebar.php'); ?>
+        <div class="maincontainer">
+            <div class="container">
+                <div class="content">
+        
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include('footer.php'); ?>
+    <script src="scripts.js"></script>
 </body>
 
 </html>
