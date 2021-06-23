0ab2039603abae90794370807c5e2cb75a0a2398
greydelta <55234616+greydelta@users.noreply.github.com>
2021-06-24 00:00:00 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -9,6 +9,44 @@
                 <div class="bar">
                     <img src="images/bars.png" alt="bars" width="50px">
                 </div>
+
+                <?php
+                    // If SESSION not Started => Guest
+                    if(!isset($_SESSION['user_type'])): ?>
+                        <div class="category">
+                            <!--Do not display Men Women Kids-->
+                        </div>
+                <?php
+                    else: 
+                ?>   
+                    <?php // Admin
+                        if($_SESSION['user_type'] == 'admin'): ?>
+                            <div class="category">
+                                <a href="men.php">MEN</a>
+                                <a href="women.php">WOMEN</a>
+                                <a href="kids.php">KIDS</a>
+                            </div>
+                    <?php // User/Member 
+                        elseif($_SESSION['user_type'] == 'user'): ?>
+                            <div class="category">
+                                <a href="men.php">MEN</a>
+                                <a href="women.php">WOMEN</a>
+                                <a href="kids.php">KIDS</a>
+                            </div>
+                    <?php endif; ?> 
+                <?php endif; ?> 
+
+                <div class="searchwrapper">
+                    <div class="wrap">
+                        <form action="" class="search">
+                            <input type="search" placeholder="Search" name="search">
+                            <img src="images/search.png" alt="search" width="30px">
+                        </form>
+                        <div class="cart">
+                            <a href="cart.php"><img src="images/cart.png" alt="cart" width="50px"></a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     
